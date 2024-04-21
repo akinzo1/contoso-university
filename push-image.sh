a1b458edf9935d788eebec4caadb5f71000c9081
Akin Okunrinboye <akinzo@hotmail.com>
2024-04-21 17:18:20 -0500

    test checkin

--- push-image.sh
+++ push-image.sh
@@ -4,8 +4,6 @@ set -eu
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 127275507218.dkr.ecr.us-east-1.amazonaws.com
 
-docker images
-
 docker tag contosouniversity:latest 127275507218.dkr.ecr.us-east-1.amazonaws.com/contosouniversity:latest
 
 docker push 127275507218.dkr.ecr.us-east-1.amazonaws.com/contosouniversity:latest
